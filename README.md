7864aba8b7362f048d28a11d5abe1bf42b5db255
Julião Langa <julitolanga@gmail.com>
2022-06-26 19:14:07 -0300

    atualiza readme.md

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ExercicioAula3js
+O Exercício Recebe dados do usuário e exibi-os no console.log()
+
+● Foi Utilizada função prompt()
+● Exibi as informações agrupadas em uma tabela com auxílio do método console.table()
--- script.js
+++ script.js
@@ -1,24 +1,22 @@
-
 let primeiroNome = prompt("Informe seu primeiro nome: ", "Julito");
 let outrosNomes = prompt("Informe seu sobreNome ", "Langa");
-let idade = parseInt(prompt("Informe sua idade: ", "97")); 
+let idade = parseInt(prompt("Informe sua idade: ", "97"));
 let endereco = prompt("Informe seu CEP ", "37500-208");
 
-const obj = { /*Create a object */
-  Nome: primeiroNome,
-  SobreNome:outrosNomes,
+const obj = {
+  /*Create a object */ Nome: primeiroNome,
+  SobreNome: outrosNomes,
   Idadde: idade,
   CEP: endereco,
 };
 
-
-console.log(`Olá ${primeiroNome} ${outrosNomes}, você informou que sua idade é de ${idade} anos. Você mora na cidade cujo CEP é ${endereco} `);
+console.log(
+  `Olá ${primeiroNome} ${outrosNomes}, você informou que sua idade é de ${idade} anos. Você mora na cidade cujo CEP é ${endereco} `
+);
 
 console.table({
   Nome: primeiroNome,
-  SobreNome:outrosNomes,
+  SobreNome: outrosNomes,
   Idadde: idade,
   CEP: endereco,
-
-})
-
+});
